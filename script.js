2c14c583c51e6ba8b28b8a1c8b6287d691a0de49
Kristin Saunders <kmsaunders7@gmail.com>
2020-12-19 17:53:30 -0600

    working current Weather status

--- script.js
+++ script.js
@@ -34,8 +34,8 @@ for (var i=0; i < cities.length; i++) {
 
 function getWeather(cityName) {
     // start with an empty field in both the current weather and five day forecast spots
-    $('.currentWeather').empty()
-    $('.forecastWeather').empty()
+    // $('.currentWeather').empty()
+    // $('.forecastWeather').empty()
 
 // Here we construct our URL using weatherAPI and the city the user inputs
     var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=26397b10a7f204a93b15533da92e9276"
@@ -45,45 +45,25 @@ function getWeather(cityName) {
         method: "GET"
     }).then(function(response) {
         console.log(response)
-        
+        // variable for differnent response calls and appending to html
+        var location = $('<h4>').text(response.name + ': ' + (moment().format('LLL')))
+        var temperature = $('<h5>').text('Temperature= ' + response.main.temp + ' *F')
+        var humidity = $('<h5>').text('Humidity= ' + response.main.humidity + '%')
+        var windSpeed = $('<h5>').text('Wind Speed= ' + response.wind.speed + 'mph')
+        // Adds Weather Icon to a variable by changing the setting the attribute to a different source
+        var setIcon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png').addClass('icon')
+        $('.currentWeather').append(location, temperature, humidity, windSpeed, setIcon)
+       
 
-        // var location = $('<h2>').text(response.name)
-        // var date = $('<h3>').text(moment().format('LLL'))
-        
-        // variable for differnent response calls 
-        // var temperature = $('<h4>').text(response.main.temp)
-        // var humidity = $('<h4>').text(response.main.humidity)
-        // var windSpeed = $('<h4>').text(response.wind.speed)
-        // var icon = $('<h4>').text(response.weather[0].icon)
-        // $('.currentWeather').append(location, temperature, date, humidity, windSpeed, icon)
-        
+    })
+}
     
-    
-
-
-  
  
-    
 
 
         // var longitude = response.coord.lon
         // var latitude = response.coord.lat
 
-
-       
-        // //create a new div to display the current weather
-        // var weatherContainer = $('<div>').addClass('current-body')
-        // var cityTitle = $('<h1>').text(location + ' (' + date + ') ')
-        // var setIcon = $('<img>').attr('src', 'http://openweathermap.org/img/w/' + icon + '.png')
-        // cityTitle.append(setIcon)
-        // weatherContainer.append(cityTitle)
-
-        // this headers will hold the text to describe the current weather, will go in the current-body div created
-        // var tempText = $('<h2>').attr('class', 'row').text('Temperature= ' + temperature + 'degrees F')
-        // weatherContainer.append(tempText)
-        // var humidityText = $('<h2>').attr('class', 'row').text('Humidity= ' + humidity + '%')
-        // weatherContainer.append(humidityText)
-        // windText = $('<h2>').attr('class', 'row').text('Wind Speed= ' + windSpeed + 'mph')
 
         //new url code for the uv index, it needs the longitude and latitude coordinates:
     //     var urlUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=26397b10a7f204a93b15533da92e9276"
