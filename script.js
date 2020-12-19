3330809b3322463407f839c287334e3700c8cef4
Kristin Saunders <kmsaunders7@gmail.com>
2020-12-18 18:15:09 -0600

    fix html format

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 
-var APIkey = "26397b10a7f204a93b15533da92e9276"
-
-
+//once the user clicks the search button this function will be called
 $("#search-city").on("click", function(event) {
     event.preventDefault();
 
     // Here we grab the text from the input box
-    var cityName= $("#city-input").val();
+    var cityName= $("#city-input").val().trim;
+    getWeather(cityName)
+    if (!localStorage.getItem(cityName)) {
+        storeButton(cityName)
+    }
+    $("#city-input").val("")
 
-    // Here we construct our URL using weatherAPI
+})  
+    //my individual api key for openweathermap
+
+var APIkey = "26397b10a7f204a93b15533da92e9276"
+
+// create function called on the click button that then calls the weather URL,
+
+function getWeather(cityName) {
+    // start with an empty field in both the current weather and five day forecast spots
+    $('.currentWeather').empty()
+    $('.forecastWeather').empty()
+
+// Here we construct our URL using weatherAPI and the city the user inputs
     var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey;
 
 
@@ -19,5 +34,3 @@ $("#search-city").on("click", function(event) {
         $("#city-view").text(JSON.stringify(response));
     });
 
-
-    });
